d6912921aaaf8c491d065553737b3797eca1c55e
Brian <odonnell.brian17@gmail.com>
2025-08-27 08:47:53 -0500

    Add enemy ships and randomly spawn ufo

--- Levels/spawn_manager.gd
+++ Levels/spawn_manager.gd
@@ -3,10 +3,25 @@ extends Node2D
 
 const fuel_scene: PackedScene = preload("res://Entities/fuel/fuel.tscn")
 const bridge_scene: PackedScene = preload("res://Entities/bridge/bridge.tscn")
-const enemy_scenes: Array[PackedScene] = [preload("res://Entities/enemy_ufo/enemy_ufo.tscn")]
+const enemy_scenes: Array[PackedScene] = [
+	preload("res://Entities/enemy_ship/enemy_ship.tscn"),
+	preload("res://Entities/enemy_ship/enemy_ship2.tscn")
+]
+const ufo_scene = preload("res://Entities/enemy_ufo/enemy_ufo.tscn")
+
+@export var ufo_spawn_time_range: Vector2 = Vector2(10.0, 30.0)
+
+@onready var ufo_timer: Timer = $UfoTimer
+
+var ufo_spawn_position: Vector2
 
 func _ready() -> void:
 	Signals.level_pieces_added.connect(spawn_entities)
+
+	# TODO: randomize spawn position
+	ufo_spawn_position = Vector2(get_viewport().size.x + 100, 200.0)
+	ufo_timer.timeout.connect(spawn_ufo)
+	start_ufo_timer()
 
 func spawn_entities(level_pieces: Array[LevelPiece]) -> void:
 	for i in range(level_pieces.size() - 1):
@@ -24,3 +39,9 @@ func spawn_entity(spawn_position: Vector2, scene: PackedScene) -> void:
 	var entity = scene.instantiate() as Node2D
 	entity.global_position = spawn_position
 	add_child(entity)
+
+func spawn_ufo() -> void:
+	spawn_entity(ufo_spawn_position, ufo_scene)
+
+func start_ufo_timer() -> void:
+	ufo_timer.start(randf_range(ufo_spawn_time_range.x, ufo_spawn_time_range.y))
